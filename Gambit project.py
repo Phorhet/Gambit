8b126e95bd9901179ff2d4b8bbbd9209503dbf9f
prophet <git-romik180607@yandex.ru>
2024-11-23 22:33:34 +0300

    Fix history list

--- Gambit project.py
+++ Gambit project.py
@@ -1,9 +1,9 @@
 import sqlite3
 import sys
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLineEdit, QPushButton, QListWidget, QTabWidget, \
-    QTextEdit, QPlainTextEdit, QLabel, QMessageBox
+    QTextEdit, QPlainTextEdit, QLabel, QMessageBox, QScrollBar, QTextBrowser
 from qt_material import apply_stylesheet
-
+from PyQt6.QtGui import QPixmap
 from PyQt6.QtCore import Qt
 
 
@@ -20,9 +20,21 @@ def show_welcome_message():
     msg.exec()
 
 
+class AnotherWindow(QWidget):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+        layout = QVBoxLayout()
+        self.text_browser = QTextBrowser()
+        self.text_browser.setText(name)
+        layout.addWidget(self.text_browser)
+        self.setLayout(layout)
+        self.setGeometry(500, 300, 400, 500)
+
 class GambitWindow(QWidget):
     def __init__(self):
         super().__init__()
+        self.w = None
         self.initUI()
 
     def get_all(self):
@@ -34,9 +46,6 @@ class GambitWindow(QWidget):
 
     def enter_pressed(self):
         self.search()
-        #print(self.search_line_edit.text())
-
-
 
 
     def initUI(self):
@@ -48,16 +57,11 @@ class GambitWindow(QWidget):
         self.search_line_edit.setClearButtonEnabled(True)
         search_button = QPushButton("Поиск")
         search_button.clicked.connect(self.search)
-        #event = Qt.Key.Key_Enter
-        #if event == Qt.Key.Key_Enter:
-            #self.on_click.search
         search_layout.addWidget(self.search_line_edit)
         search_layout.addWidget(search_button)
-        self.history_list = QListWidget()
+        self.history_list = QTextBrowser()
         search_layout.addWidget(self.history_list)
         search_tab.setLayout(search_layout)
-
-
         # таб для сохранения
         info_tab = QWidget()
         info_layout = QVBoxLayout()
@@ -72,7 +76,6 @@ class GambitWindow(QWidget):
         info_layout.addWidget(self.save_area_edit)
         info_layout.addWidget(save_button)
         info_tab.setLayout(info_layout)
-
         # таб гамбиты
         gambits_tab = QWidget()
         gambits_layout = QVBoxLayout()
@@ -80,10 +83,11 @@ class GambitWindow(QWidget):
         self.gambits_list.addItems(self.get_all())
         gambits_layout.addWidget(self.gambits_list)
         gambits_tab.setLayout(gambits_layout)
+        self.gambits_list.itemDoubleClicked.connect(self.doubleclickmouse)
+        #Табы и лаяут
         tabs.addTab(search_tab, "Поиск")
         tabs.addTab(info_tab, "Добавление данных")
         tabs.addTab(gambits_tab, "Гамбиты")
-
         main_layout = QVBoxLayout()
         main_layout.addWidget(tabs)
         self.setLayout(main_layout)
@@ -97,18 +101,29 @@ class GambitWindow(QWidget):
         result = cur.execute("""select * from gambit where name like ?""", (search_text,)).fetchone()
         print(result[2])
         self.history_list.clear()
-        self.history_list.addItem(result[2])
-
+        self.history_list.setText(result[2])
 
     def save_base(self):
         save_textfl = self.save_line_edit.text()
         save_textsl = self.save_area_edit.toPlainText()
         try:
-            save = cur.execute("""INSERT INTO gambit(name, description) VALUES(?,?); """, (save_textfl, save_textsl))
+            save = cur.execute(""""INSERT INTO gambit(name, description) VALUES(?,?); """, (save_textfl, save_textsl))
             con.commit()
             print(cur.lastrowid)
         except:
             print("something went wrong")
+
+    def doubleclickmouse(self, item):
+        clicked_name = item.text()
+        result = cur.execute(
+            """select * from gambit where name = ?""",
+            (clicked_name,)
+        ).fetchone()
+        if result is not None:
+            self.w = AnotherWindow(result[2])
+            self.w.show()
+        else:
+            QMessageBox.warning(self, "Ошибка", "Гамбит не найден")
 
 
 if __name__ == '__main__':
@@ -117,3 +132,7 @@ if __name__ == '__main__':
     show_welcome_message()
     ex = GambitWindow()
     sys.exit(app.exec())
+
+
+
+
